caaf2203e9d009088d2d24fa42e94bf6ef37fdcc
Matthew Caruana Galizia <mattcg@gmail.com>
2013-03-19 16:49:06 -0600

    Allow includes to be set

--- src/MattCG/cjsDeliveryBundle/Assetic/Filter/cjsDeliveryFilter.php
+++ src/MattCG/cjsDeliveryBundle/Assetic/Filter/cjsDeliveryFilter.php
@@ -12,6 +12,7 @@ class cjsDeliveryFilter implements FilterInterface {
 	const EXT_JS = 'js';
 
 	private $minifyidentifiers = false;
+	private $includes = null;
 
 	private function stripExtension($filepath) {
 		return preg_replace('/\.' . self::EXT_JS . '$/', '', $filepath);
@@ -21,13 +22,21 @@ class cjsDeliveryFilter implements FilterInterface {
 		$this->minifyidentifiers = $minifyidentifiers;
 	}
 
+	public function setIncludes($includes) {
+		$this->includes = $includes;
+	}
+
 	public function filterLoad(AssetInterface $asset) {}
 
 	public function filterDump(AssetInterface $asset) {
 		$filepath = $asset->getSourceRoot() . '/' . $asset->getSourcePath();
 		$moduleidentifier = $this->stripExtension($filepath);
 
-		$delivery = DeliveryFactory::create(array('minifyIdentifiers' => $this->minifyidentifiers));
+		$options = array();
+		$options['includes'] = $this->includes;
+		$options['minifyIdentifiers'] = $this->minifyidentifiers;
+
+		$delivery = DeliveryFactory::create($options);
 		$delivery->addModule($moduleidentifier, $asset->getContent());
 		$delivery->setMainModule($moduleidentifier);
 
